3f4279d6e1cbfe56eb8a951e216bd9a884af3ace
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-03 13:24:53 +0100

    Document :(

--- test/LowRISCTaggedMemory/VPtrTagging/build.sh
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# FIXME Build it all with clang when it works! :(
 echo Building main.cc with gcc
 riscv64-unknown-elf-gcc -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S main.cc || exit 1
 for x in Test SubclassTest; do 
