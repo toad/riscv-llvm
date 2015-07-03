2d5b2e94946aaeec5ebbdb5e51c31080f1d58785
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-03 13:24:06 +0100

    Add build script.wq

---
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Building main.cc with gcc
+riscv64-unknown-elf-gcc -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S main.cc || exit 1
+for x in Test SubclassTest; do 
+echo Building ${x}.cc with clang
+clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc || exit 2
+done
+echo Assembling and linking with gcc
+riscv64-unknown-elf-g++ -o test.riscv *.s || exit 3
+echo Success!
