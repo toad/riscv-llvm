0385612b6fa5f4622b2c6c31f4dc1c677f5ddc82
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-06 10:31:05 +0100

    Rename, refactor build script, will be more main's

--- test/LowRISCTaggedMemory/VPtrTagging/build.sh
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # FIXME Build it all with clang when it works! :(
-echo Building main.cc with gcc
-riscv64-unknown-elf-gcc -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S main.cc || exit 1
-for x in Test SubclassTest; do 
-echo Building ${x}.cc with clang
-clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc || exit 2
+for main in main-read-only-vptr.cc; do
+	echo Building $main with gcc
+	riscv64-unknown-elf-gcc -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main || exit 1
+	#clang -O1 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main || exit 1
+	for x in Test SubclassTest; do 
+		echo Building ${x}.cc with clang
+		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc || exit 2
+	done
+	echo Assembling and linking with gcc
+	riscv64-unknown-elf-g++ -o test-${main}.riscv *.s || exit 3
+	echo Success!
 done
-echo Assembling and linking with gcc
-riscv64-unknown-elf-g++ -o test.riscv *.s || exit 3
-echo Success!
