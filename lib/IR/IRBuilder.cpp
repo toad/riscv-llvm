01eab1891da960c65adb97f5c7e62e947b1a9f0d
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-07 15:23:35 +0100

    Fix enum

--- lib/IR/IRBuilder.cpp
+++ lib/IR/IRBuilder.cpp
@@ -194,7 +194,7 @@ Function *IRBuilderBase::lazyGetRISCVLoadTaggedReadOnly(Module *m) {
   BasicBlock* onNotTagged = BasicBlock::Create(getGlobalContext(), "entry", f);
   IRBuilder<> builder(entry);
   Value *ltag = builder.CreateRISCVLoadTag(ptr);
-  Value *ltagEqualsOne = builder.CreateICmpEQ(ltag, getInt64(LowRISCMemoryTag.CLEAN));
+  Value *ltagEqualsOne = builder.CreateICmpEQ(ltag, getInt64(TAG_CLEAN));
   builder.CreateCondBr(ltagEqualsOne, onTagged, onNotTagged);
   builder.SetInsertPoint(onTagged);
   builder.CreateRetVoid();
