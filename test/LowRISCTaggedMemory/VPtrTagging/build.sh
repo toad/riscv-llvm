8f2ae50771490a5e8b8e7c8d52943fadf16d1ca3
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-07 16:19:09 +0100

    Build the classes once only

--- test/LowRISCTaggedMemory/VPtrTagging/build.sh
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # FIXME Build it all with clang when it works! :(
+rm -f *.s
+for x in Test SubclassTest; do
+	echo Building ${x}.cc with clang
+	clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc || exit 2
+done
 for main in main-*.cc; do
-	rm -f *.s
 	case $main in
 		main-vtable-replacement.cc)
 			build=gcc
@@ -13,6 +17,7 @@ for main in main-*.cc; do
 			build=clang
 			;;
 	esac
+	rm -f main*.s
 	if test "$build" = "gcc"; then
 		echo Building $main with gcc
 		riscv64-unknown-elf-gcc -fpermissive -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main || exit 1
@@ -20,11 +25,7 @@ for main in main-*.cc; do
 		echo Building $main with clang
 		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main || exit 1
 	fi
-	for x in Test SubclassTest; do 
-		echo Building ${x}.cc with clang
-		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc || exit 2
-	done
 	echo Assembling and linking with gcc
-	riscv64-unknown-elf-g++ -o test-${main}.riscv *.s || exit 3
+	riscv64-unknown-elf-g++ -o test-${main}.riscv Test.s SubclassTest.s main*.s || exit 3
 	echo Success!
 done
