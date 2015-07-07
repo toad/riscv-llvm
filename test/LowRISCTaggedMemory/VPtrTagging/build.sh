a6bf1e461aa8684a7dbfe3bbb7f4f52b3e55afe4
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-07 16:22:44 +0100

    Try to build them all with both

--- test/LowRISCTaggedMemory/VPtrTagging/build.sh
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -6,26 +6,18 @@ for x in Test SubclassTest; do
 	clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc || exit 2
 done
 for main in main-*.cc; do
-	case $main in
-		main-vtable-replacement.cc)
-			build=gcc
-			;;
-		main-read-only-vptr.cc)
-			build=gcc
-			;;
-		*)
-			build=clang
-			;;
-	esac
-	rm -f main*.s
-	if test "$build" = "gcc"; then
-		echo Building $main with gcc
-		riscv64-unknown-elf-gcc -fpermissive -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main || exit 1
-	else
-		echo Building $main with clang
-		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main || exit 1
-	fi
-	echo Assembling and linking with gcc
-	riscv64-unknown-elf-g++ -o test-${main}.riscv Test.s SubclassTest.s main*.s || exit 3
-	echo Success!
+	for build in gcc clang
+	do
+		rm -f main*.s
+		if test "$build" = "gcc"; then
+			echo Building $main with gcc
+			riscv64-unknown-elf-gcc -fpermissive -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main || exit 1
+		else
+			echo Building $main with clang
+			if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main; then echo Failed to build $main with $build; break; fi
+		fi
+		echo Assembling and linking with gcc
+		riscv64-unknown-elf-g++ -o test-${main}.${build}.riscv Test.s SubclassTest.s main*.s || exit 3
+		echo Successfully built test-${main}.${build}.riscv
+	done
 done
