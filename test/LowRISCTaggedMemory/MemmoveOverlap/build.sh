17679e813504ad0b7b754c58f6aae62b9eb26fb2
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-15 11:59:23 +0100

    Test newlib *and* LLVM (make size configurable)

--- test/LowRISCTaggedMemory/MemmoveOverlap/build.sh
+++ test/LowRISCTaggedMemory/MemmoveOverlap/build.sh
@@ -2,7 +2,8 @@
 # FIXME Build it all with clang when it works! :(
 rm -f *.s *.ll *.riscv *.bc
 x=main
-clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I -I $RISCV/riscv64-unknown-elf/include/ -S ${x}.c -emit-llvm -DARRAY_SIZE=$ARRAY_SIZE -o ${x}.ll || exit 2
+LENGTH=${1:-3}
+clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I -I $RISCV/riscv64-unknown-elf/include/ -S ${x}.c -emit-llvm -DLENGTH=$LENGTH -o ${x}.ll || exit 2
 opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.bc || exit 3
 llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
 riscv64-unknown-elf-g++ -o $x.riscv *.s || exit 5
--- test/LowRISCTaggedMemory/MemmoveOverlap/main.c
+++ test/LowRISCTaggedMemory/MemmoveOverlap/main.c
@@ -4,23 +4,35 @@
 #include "tag.h"
 
 #define LAZY 4
+#ifndef LENGTH
+#define LENGTH 3
+#else
+# if LENGTH < 3
+#  error LENGTH must be at least 3
+# endif
+#endif
 
-void main() {
-	long arr[] = { 1, 2, 3 };
+int main() {
+	long arr[LENGTH];
+	int i=0;
+	for(i=0;i<LENGTH;i++) arr[i] = i;
 	store_tag(&arr[0], LAZY);
 	store_tag(&arr[1], LAZY);
-	memmove(&arr[1], &arr[0], sizeof(long)*2);
-	if(arr[1] != 1 && arr[2] != 2) {
-		printf("ERROR: array is now %ld %ld %ld\n", arr[0], arr[1], arr[2]);
-		exit(1);
-	}
+	memmove(&arr[1], &arr[0], sizeof(long)*(LENGTH-1));
 	if(load_tag(&arr[1]) != LAZY) {
-		printf("ERROR: Tag on [1] should be LAZY=%d, is %d", LAZY, load_tag(&arr[1]));
+		printf("ERROR: Tag on [1] should be LAZY=%d, is %d\n", LAZY, load_tag(&arr[1]));
 		exit(2);
 	}
 	if(load_tag(&arr[2]) != LAZY) {
-		printf("ERROR: Tag on [2] should be LAZY=%d, is %d", LAZY, load_tag(&arr[2]));
+		printf("ERROR: Tag on [2] should be LAZY=%d, is %d\n", LAZY, load_tag(&arr[2]));
 		exit(3);
 	}
+	for(i=1;i<LENGTH;i++) {
+		if(arr[i] != i-1) {
+			printf("Array[%d]=%ld should be %d\n", i, arr[i], i-1);
+			exit(4);
+		}
+	}
 	printf("Success!\n");
+	return 0;
 }
