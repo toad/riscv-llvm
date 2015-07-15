53e5ef6accfd3e2be7cd981e66bc2a007ca38399
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-15 14:29:09 +0100

    Missing file

---
+++ test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build-and-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for x in $(seq 0 1000)
+do
+	echo Testing non-overlapping memmove and memcpy with size $x
+	./build.sh $x > /dev/null 2>&1 || exit 1
+	if ! spike pk test-main.c.gcc.riscv | grep Success > /dev/null;
+	then echo Failed with GCC with size $x; fi # Keep going
+	if ! spike pk test-main.c.clang.riscv | grep Success > /dev/null;
+	then
+		spike pk test-main.c.clang.riscv
+		echo Failed with Clang with size $x
+		exit 2
+	fi
+done
