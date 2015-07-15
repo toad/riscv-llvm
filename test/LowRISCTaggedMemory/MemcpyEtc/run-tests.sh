f88c183cd526f93ebde8c3e3acad0d108e6aaa66
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-15 14:05:33 +0100

    Use both tests

--- test/LowRISCTaggedMemory/MemcpyEtc/run-tests.sh
+++ test/LowRISCTaggedMemory/MemcpyEtc/run-tests.sh
@@ -4,5 +4,7 @@ cd Memcpy
 if ! ./build-and-test.sh; then echo Failed memcpy/memmove test; exit 2; fi
 cd ..
 cd MemmoveOverlap
-if ! ./build-and-test.sh; then echo Failed memmove overlapped tags test; exit 3; fi
+if ! ./build-and-test.sh; then echo Failed backwards memmove overlapped tags test; exit 3; fi
+cd MemmoveOverlapForwards
+if ! ./build-and-test.sh; then echo Failed forwards memmove overlapped tags test; exit 3; fi
 echo All tests successful.
