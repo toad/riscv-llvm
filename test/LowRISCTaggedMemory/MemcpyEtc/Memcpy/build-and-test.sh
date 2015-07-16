60ff25035f7db31296c1099ba7dce7771d049db0
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-16 09:38:14 +0100

    build.sh parameter for gcc or clang

--- test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build-and-test.sh
+++ test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build-and-test.sh
@@ -2,9 +2,10 @@
 for x in $(seq 0 1000)
 do
 	echo Testing non-overlapping memmove and memcpy with size $x
-	./build.sh $x > /dev/null 2>&1 || exit 1
-	if ! spike pk test-main.c.gcc.riscv | grep Success > /dev/null;
-	then echo Failed with GCC with size $x; fi # Keep going
+#	./build.sh gcc $x > /dev/null 2>&1 || exit 1
+#	if ! spike pk test-main.c.gcc.riscv | grep Success > /dev/null;
+#	then echo Failed with GCC with size $x; fi # Keep going
+	./build.sh clang $x > /dev/null 2>&1 || exit 1
 	if ! spike pk test-main.c.clang.riscv | grep Success > /dev/null;
 	then
 		spike pk test-main.c.clang.riscv
--- test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build.sh
+++ test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# FIXME Build it all with clang when it works! :(
+build=${1:-clang}
 rm -f *.s *.ll *.riscv *.bc
 ARRAY_SIZE=${1:-10000}
 for x in object; do
-	echo Building ${x}.c with clang
-	clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I -I $RISCV/riscv64-unknown-elf/include/ -S ${x}.c -emit-llvm -DARRAY_SIZE=$ARRAY_SIZE -o ${x}.ll || exit 2
-	opt -load $TOP/riscv-llvm/build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.bc || exit 3
-	llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
+	if test "$build" == "clang"; then
+		echo Building ${x}.c with clang
+		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I -I $RISCV/riscv64-unknown-elf/include/ -S ${x}.c -emit-llvm -DARRAY_SIZE=$ARRAY_SIZE -o ${x}.ll || exit 2
+		opt -load $TOP/riscv-llvm/build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.bc || exit 3
+		llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
+	else
+		riscv64-unknown-elf-gcc -O0 -I $RISCV/riscv-linux/include/ -S ${x}.c -DARRAY_SIZE=$ARRAY_SIZE -o ${x}.s || exit 2
+	fi
 done
 for main in main*.c; do
 	for build in gcc clang
