75b061c1e2ed0a45e8e1330af5db0d56bc92ad91
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-16 10:21:55 +0100

    Factor out variables

--- test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build.sh
+++ test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 build=${1:-clang}
-rm -f *.s *.ll *.riscv *.bc
 ARRAY_SIZE=${1:-10000}
+VARIABLES="-DARRAY_SIZE=$ARRAY_SIZE"
+rm -f *.s *.ll *.riscv *.bc
 for x in object tag; do
 	case "$build" in
 	"clang")
 		echo Building ${x}.c with clang
-		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I. -I $RISCV/riscv64-unknown-elf/include/ -S ${x}.c -emit-llvm -DARRAY_SIZE=$ARRAY_SIZE -o ${x}.ll || exit 2
+		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I. -I $RISCV/riscv64-unknown-elf/include/ -S ${x}.c -emit-llvm $VARIABLES -o ${x}.ll || exit 2
 		opt -load $TOP/riscv-llvm/build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.bc || exit 3
 		llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
 		;;
 	"gcc")
-		riscv64-unknown-elf-gcc -O0 -I $RISCV/riscv-linux/include/ -S ${x}.c -DARRAY_SIZE=$ARRAY_SIZE -o ${x}.s || exit 2
+		riscv64-unknown-elf-gcc -O0 -I $RISCV/riscv-linux/include/ -S ${x}.c $VARIABLES -o ${x}.s || exit 2
 		;;
 	*)
 		exit 5
@@ -22,11 +23,11 @@ for main in main*.c; do
 	case "$build" in
 	"gcc")
 		echo Building $main with gcc
-		riscv64-unknown-elf-gcc -fpermissive -O0 -I $RISCV/riscv64-unknown-elf/include/ -S -DARRAY_SIZE=$ARRAY_SIZE $main -o ${main}.s || exit 1
+		riscv64-unknown-elf-gcc -fpermissive -O0 -I $RISCV/riscv64-unknown-elf/include/ -S $VARIABLES $main -o ${main}.s || exit 1
 	;;
 	"clang")
 		echo Building $main with clang
-		if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I. -I $RISCV/riscv64-unknown-elf/include/ -S -DARRAY_SIZE=$ARRAY_SIZE $main -emit-llvm -o ${main}.ll; then echo Failed to build $main with $build; break; fi
+		if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I. -I $RISCV/riscv64-unknown-elf/include/ -S $VARIABLES $main -emit-llvm -o ${main}.ll; then echo Failed to build $main with $build; break; fi
 		if ! opt -load $TOP/riscv-llvm/build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${main}.ll > ${main}.opt.bc ; then echo Failed to optimise $main; break; fi
 		if ! llc -filetype=asm -march=riscv -mcpu=LowRISC ${main}.opt.bc -o ${main}.opt.s; then echo Failed to convert optimised $main to assembler; break; fi
 		;;
