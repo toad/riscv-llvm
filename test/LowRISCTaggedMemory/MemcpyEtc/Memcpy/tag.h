e7cd74a1011fa2278368800335d8a00b8df2db65
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-16 10:19:01 +0100

    Will use tags. (Not inline)
    Not inline because of bugs in Clang and problems with GCC.

---
+++ test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/tag.h
@@ -0,0 +1,14 @@
+// See LICENSE for license details.
+
+#ifndef LOWRISC_TAG_H
+#define LOWRISC_TAG_H
+
+#define TAG_WIDTH 4
+
+/* FIXME these are not inline because it breaks clang if they are! */
+
+int load_tag(void *addr);
+
+void store_tag(void *addr, int tag);
+
+#endif
