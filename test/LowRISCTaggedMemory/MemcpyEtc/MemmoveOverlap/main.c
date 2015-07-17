4e102088e4ed422230e981303b0dfabcbd373e34
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-17 11:09:40 +0100

    Tag the ends of the array as read-only
    Checks for writing over end.

--- test/LowRISCTaggedMemory/MemcpyEtc/MemmoveOverlap/main.c
+++ test/LowRISCTaggedMemory/MemcpyEtc/MemmoveOverlap/main.c
@@ -12,19 +12,25 @@
 #endif
 
 int main() {
-	long arr[LENGTH];
+	printf("Testing overlapped memmove with LENGTH=%d\n", LENGTH);
+	long realarray[LENGTH + 2];
+	long* arr=&realarray[1];
+	store_tag(&realarray[0], READ_ONLY);
+	store_tag(&realarray[LENGTH+1], READ_ONLY);
 	int i=0;
 	for(i=0;i<LENGTH;i++) arr[i] = i;
 	store_tag(&arr[0], LAZY);
 	store_tag(&arr[1], LAZY);
 	printf("Before move:\n");
-        for(i=0;i<LENGTH;i++) {
-                printf("Array[%d]=%ld tag %d\n", i, arr[i], load_tag(&arr[i]));
+        for(i=0;i<LENGTH+2;i++) {
+                printf("Array[%d]=%ld tag %d at %p\n", i, realarray[i], load_tag(&realarray[i]), &realarray[i]);
 	}
+	printf("\n");
 	memmove(&arr[1], &arr[0], sizeof(long)*(LENGTH-1));
-	printf("Array[0]=%ld tag %d\n", arr[0], load_tag(&arr[0]));
+	for(i=0;i<LENGTH+2;i++) {
+		printf("Array[%d]=%ld tag %d\n", i, realarray[i], load_tag(&realarray[i]));
+	}
 	for(i=1;i<LENGTH;i++) {
-		printf("Array[%d]=%ld tag %d should be %d\n", i, arr[i], load_tag(&arr[i]), i-1);
 		if(arr[i] != i-1) {
 			printf("Array[%d]=%ld should be %d\n", i, arr[i], i-1);
 			printf("Dump:\n\n");
@@ -42,6 +48,7 @@ int main() {
 		printf("ERROR: Tag on [2] should be LAZY=%d, is %d\n", LAZY, load_tag(&arr[2]));
 		exit(3);
 	}
-	printf("Success!\n");
+	for(i=0;i<LENGTH+2;i++) store_tag(&realarray[i], 0);
+	printf("Success!\n\n");
 	return 0;
 }
