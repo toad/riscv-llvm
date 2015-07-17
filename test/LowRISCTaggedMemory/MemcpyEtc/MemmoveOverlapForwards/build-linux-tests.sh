a37f36ae5c1b13c0ff3d54f5550384b6cd3bf534
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-17 11:45:59 +0100

    Add test generator here too

---
+++ test/LowRISCTaggedMemory/MemcpyEtc/MemmoveOverlapForwards/build-linux-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# $1 = compiler type
+# $2 = array size
+SCRIPT=run-memmove-overlap-forward-tests.sh
+build()
+{
+	if ! ./build-gcc-linux.sh $1 > /dev/null 2>&1
+	then echo Could not build with gcc-linux $1; exit 1; fi
+}
+
+rm -Rf mnt
+mkdir mnt
+mkdir mnt/bin
+rm -f *.riscv-linux
+rm $SCRIPT
+echo "#!/bin/ash" > run-memcpy-tests.sh
+for x in $(seq 3 100) $(seq 2000 2100)
+do
+	echo Building $x
+	build $x
+	mv main.riscv-linux mnt/bin/memmove-overlap-$x
+	echo "echo Running test $x" >> $SCRIPT
+	echo "memmove-overlap-$x || exit" >> $SCRIPT
+done
+echo "echo Completed all tests" >> $SCRIPT
+chmod +x $SCRIPT
