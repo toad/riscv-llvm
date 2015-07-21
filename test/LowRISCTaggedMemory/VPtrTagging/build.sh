00e89d29adeef3b06832c19919f5a6387c45e4e0
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-21 14:04:09 +0100

    Fix filenames (.bc not .ll)

--- test/LowRISCTaggedMemory/VPtrTagging/build.sh
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -4,8 +4,8 @@ rm -f *.s *.ll *.riscv
 for x in Test SubclassTest; do
 	echo Building ${x}.cc with clang
 	clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc -emit-llvm -o ${x}.ll -fno-exceptions || exit 2
-	opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.ll || exit 3
-	llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.ll -o ${x}.opt.s || exit 4
+	opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.bc || exit 3
+	llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
 done
 for main in main-*.cc; do
 	for build in gcc clang
@@ -17,8 +17,8 @@ for main in main-*.cc; do
 		else
 			echo Building $main with clang
 			if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main -emit-llvm -o ${main}.ll -fno-exceptions; then echo Failed to build $main with $build; break; fi
-			if ! opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${main}.ll > ${main}.opt.ll ; then echo Failed to optimise $main; break; fi
-			if ! llc -filetype=asm -march=riscv -mcpu=LowRISC ${main}.opt.ll -o ${main}.opt.s; then echo Failed to convert optimised $main to assembler; break; fi
+			if ! opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${main}.ll > ${main}.opt.bc ; then echo Failed to optimise $main; break; fi
+			if ! llc -filetype=asm -march=riscv -mcpu=LowRISC ${main}.opt.bc -o ${main}.opt.s; then echo Failed to convert optimised $main to assembler; break; fi
 		fi
 		echo Assembling and linking with gcc
 		riscv64-unknown-elf-g++ -o test-${main}.${build}.riscv *.s || exit 3
