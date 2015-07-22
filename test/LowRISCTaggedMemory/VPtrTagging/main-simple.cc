d6a2ab54659d2c1c22f90a3204ef1b076adacc48
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-22 10:22:39 +0100

    Logging, optionally check for tags.

--- test/LowRISCTaggedMemory/VPtrTagging/main-simple.cc
+++ test/LowRISCTaggedMemory/VPtrTagging/main-simple.cc
@@ -3,6 +3,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cassert>
+
+#ifdef CHECK_TAGS
+#include "tag.c"
+#endif
 
 int main() {
 	Test *t;
@@ -11,6 +16,16 @@ int main() {
 	} else {
 		t = new Test(1);
 	}
+	printf("Constructed something...\n\n");
+	printf("Constructed object at %p\n", (void*)t);
+#ifdef CHECK_TAGS
+	assert(load_tag((void*)t) == LAZY_TAG && "Object vtable pointer is tagged");
+	void *vtable = *((void**) t);
+	printf("Vtable is at %p\n", vtable);
+	assert(load_tag((void*)vtable) == LAZY_TAG && "Object vtable first entry is tagged");
+#endif
+	printf("Calling method on object...\n");
 	t -> print();
+	printf("Success.\n\n");
 	return 0;
 }
