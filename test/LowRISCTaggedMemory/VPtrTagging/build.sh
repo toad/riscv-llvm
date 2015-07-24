e4d1480b7ccacab8901d3ad058a9b8d795d2b7c5
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-24 10:31:39 +0100

    Need -use-init-array

--- test/LowRISCTaggedMemory/VPtrTagging/build.sh
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -6,7 +6,7 @@ for x in Test SubclassTest; do
 	clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc -emit-llvm -o ${x}.ll -fno-exceptions || exit 2
 	opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.bc || exit 3
 	llvm-dis ${x}.opt.bc > ${x}.opt.ll
-	llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
+	llc -use-init-array -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
 done
 for main in main-*.cc; do
 	for build in gcc clang
@@ -20,7 +20,7 @@ for main in main-*.cc; do
 			if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main -emit-llvm -o ${main}.ll -fno-exceptions; then echo Failed to build $main with $build; break; fi
 			if ! opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${main}.ll > ${main}.opt.bc ; then echo Failed to optimise $main; break; fi
 		        llvm-dis ${main}.opt.bc > ${main}.opt.ll
-			if ! llc -filetype=asm -march=riscv -mcpu=LowRISC ${main}.opt.bc -o ${main}.opt.s; then echo Failed to convert optimised $main to assembler; break; fi
+			if ! llc -use-init-array -filetype=asm -march=riscv -mcpu=LowRISC ${main}.opt.bc -o ${main}.opt.s; then echo Failed to convert optimised $main to assembler; break; fi
 		fi
 		echo Assembling and linking with gcc
 		riscv64-unknown-elf-g++ -o test-${main}.${build}.riscv *.s || exit 3
