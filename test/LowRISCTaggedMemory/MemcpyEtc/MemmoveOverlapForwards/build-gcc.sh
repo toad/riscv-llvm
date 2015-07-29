139edb4918848ce94e0f4e33dcae9fed5bea6e5b
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-29 14:16:42 +0100

    Pass in -D__TAGGED_MEMORY__ on command line (for now)
    GCC should provide this in future.

--- test/LowRISCTaggedMemory/MemcpyEtc/MemmoveOverlapForwards/build-gcc.sh
+++ test/LowRISCTaggedMemory/MemcpyEtc/MemmoveOverlapForwards/build-gcc.sh
@@ -3,4 +3,4 @@
 rm -f *.s *.ll *.riscv *.bc
 x=main
 LENGTH=${1:-3}
-riscv64-unknown-elf-gcc -O0 -I . -I $RISCV/riscv64-unknown-elf/include/ ${x}.c -o $x.riscv -DLENGTH=$LENGTH || exit 5
+riscv64-unknown-elf-gcc -O0 -I . -I $RISCV/riscv64-unknown-elf/include/ ${x}.c -o $x.riscv -DLENGTH=$LENGTH -D__TAGGED_MEMORY__ || exit 5
