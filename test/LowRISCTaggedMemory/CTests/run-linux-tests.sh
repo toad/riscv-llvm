377c21be60dc8b207c4f473aeef4db0b1dc3f117
Matthew Toseland <mjt92@cam.ac.uk>
2015-08-06 10:43:15 +0100

    Test automation for Linux GCC

---
+++ test/LowRISCTaggedMemory/CTests/run-linux-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if ! ./build.sh gcc-linux; then echo Failed to build; exit 1; fi
+for x in main*.c
+do
+	if ! $TOP/lowrisc-chip/riscv-tools/run_test_linux_spike.sh $PWD/test-${x}.gcc-linux.riscv-linux
+	then
+		echo Test failed: $x
+		exit 2
+	fi
+	echo Test $x succeeded as expected
+done
+for x in fail*.c
+do
+	echo Test $x should fail with gcc...
+	if $TOP/lowrisc-chip/riscv-tools/run_test_linux_spike.sh $PWD/test-${x}.gcc-linux.riscv-linux
+	then
+		echo Test should have failed but succeeded: $x
+		exit 3
+	fi
+	echo Test $x failed with gcc as expected
+done
+echo All C tests successful
