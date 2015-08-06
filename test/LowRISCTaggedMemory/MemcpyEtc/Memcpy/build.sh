cc6f41f2fa0549d50bf646589453b60d069b509f
Matthew Toseland <mjt92@cam.ac.uk>
2015-08-06 13:11:14 +0100

    Build on clang-linux

--- test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build.sh
+++ test/LowRISCTaggedMemory/MemcpyEtc/Memcpy/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ARRAY_SIZE=${2:-10000}
 VARIABLES="-DARRAY_SIZE=$ARRAY_SIZE"
+INCLUDES_NEWLIB="-I. -I $RISCV/riscv64-unknown-elf/include/"
+INCLUDES_GLIBC="-I $RISCV/sysroot/usr/include/"
 case $1 in
 	clang)
 		# Build with Clang, with full tag support.
@@ -32,6 +34,10 @@ case $1 in
 		build=gcc-linux
 		VARIABLES="$VARIABLES -DFAKE_TAGS -D__TAGGED_MEMORY__"
 		;;
+	clang-linux)
+		# Build with Clang for a booted Linux system with full tag support
+		build=clang-linux
+		;;
 	*)
 		echo "./build.sh BUILDTYPE"
 		echo "Where BUILDTYPE is:"
@@ -40,14 +46,20 @@ case $1 in
 		echo "  gcc-force-tags: Test GCC with setting tags manually"
 		echo "  gcc-fail-tags: Test GCC with tags (should fail)"
 		echo "  gcc-linux: Build test for booted Linux using GCC with fake tags (should succeed)"
+		echo "  clang-linux: Build test for booted Linux using Clang with tags"
 		exit 1
 esac
 rm -f *.s *.ll *.bc
 for x in object; do
 	case "$build" in
-	"clang")
+	"clang"|"clang-linux")
 		echo Building ${x}.c with clang
-		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I. -I $RISCV/riscv64-unknown-elf/include/ -S ${x}.c -emit-llvm $VARIABLES -o ${x}.ll || exit 2
+		if test "$build" = "clang"; then
+			INCLUDES="$INCLUDES_NEWLIB"
+		else
+			INCLUDES="$INCLUDES_GLIBC"
+		fi
+		clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC $INCLUDES -S ${x}.c -emit-llvm $VARIABLES -o ${x}.ll || exit 2
 		opt -load $TOP/riscv-llvm/build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${x}.ll > ${x}.opt.bc || exit 3
 		llc -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
 		;;
@@ -71,9 +83,15 @@ for main in main*.c; do
 	"gcc-linux")
 		# Do nothing.
 		;;
-	"clang")
+	"clang"|"clang-linux")
+		echo Building ${x}.c with clang
+		if test "$build" = "clang"; then
+			INCLUDES="$INCLUDES_NEWLIB"
+		else
+			INCLUDES="$INCLUDES_GLIBC"
+		fi
 		echo Building $main with clang
-		if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I. -I $RISCV/riscv64-unknown-elf/include/ -S $VARIABLES $main -emit-llvm -o ${main}.ll; then echo Failed to build $main with $build; break; fi
+		if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC $INCLUDES -S $VARIABLES $main -emit-llvm -o ${main}.ll; then echo Failed to build $main with $build; break; fi
 		if ! opt -load $TOP/riscv-llvm/build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${main}.ll > ${main}.opt.bc ; then echo Failed to optimise $main; break; fi
 		if ! llc -filetype=asm -march=riscv -mcpu=LowRISC ${main}.opt.bc -o ${main}.opt.s; then echo Failed to convert optimised $main to assembler; break; fi
 		;;
@@ -85,6 +103,9 @@ for main in main*.c; do
 	if test "$build" == "gcc-linux"
 	then
 		riscv64-unknown-linux-gnu-gcc -static -o test-${main}.$1.${ARRAY_SIZE}.riscv-linux -O0 -I $RISCV/riscv-linux/include/ $VARIABLES main.c object.c || exit 3
+	elif test "$build" == "clang-linux"
+	then
+		riscv64-unknown-linux-gnu-gcc -static -o test-${main}.$1.riscv-linux $VARIABLES *.s || exit 3
 	else
 		riscv64-unknown-elf-gcc -o test-${main}.$1.${ARRAY_SIZE}.riscv *.s || exit 3
 	fi
