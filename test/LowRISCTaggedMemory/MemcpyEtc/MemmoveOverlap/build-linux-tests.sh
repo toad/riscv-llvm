d28dd8dc5ffb0027a9185ec79aa57495c8e5124c
Matthew Toseland <mjt92@cam.ac.uk>
2015-08-06 16:15:10 +0100

    Build memmove test for clang-linux

--- test/LowRISCTaggedMemory/MemcpyEtc/MemmoveOverlap/build-linux-tests.sh
+++ test/LowRISCTaggedMemory/MemcpyEtc/MemmoveOverlap/build-linux-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+compiler=${1:-gcc-linux}
 # $1 = compiler type
 # $2 = array size
 SCRIPT=run-memmove-overlap-tests.sh
 build()
 {
-	if ! ./build-gcc-linux.sh $1 > /dev/null 2>&1
+	if ! ./build-${compiler}.sh $1 > /dev/null 2>&1
 	then echo Could not build with gcc-linux $1; exit 1; fi
 }
 
@@ -16,11 +17,11 @@ rm -f $SCRIPT
 echo "#!/bin/ash" > $SCRIPT
 for x in $(seq 3 100) $(seq 2000 2100)
 do
-	echo Building $x
+	echo Building $x with $compiler
 	build $x
-	mv main.riscv-linux mnt/bin/memmove-overlap-$x
-	echo "echo Running test $x" >> $SCRIPT
-	echo "memmove-overlap-$x || exit" >> $SCRIPT
+	mv main.${compiler}.riscv-linux mnt/bin/memmove-overlap-${compiler}-$x
+	echo "echo Running test $x for $compiler" >> $SCRIPT
+	echo "memmove-overlap-${compiler}-$x || exit" >> $SCRIPT
 done
 echo "echo Completed all tests" >> $SCRIPT
 chmod +x $SCRIPT
