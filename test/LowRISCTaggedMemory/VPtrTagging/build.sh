575fd01862fe27fd5f926e61a99750193fe857b0
Matthew Toseland <mjt92@cam.ac.uk>
2015-08-07 11:17:55 +0100

    Support clang-linux and correct includes for both

--- test/LowRISCTaggedMemory/VPtrTagging/build.sh
+++ test/LowRISCTaggedMemory/VPtrTagging/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # FIXME Build it all with clang when it works! :(
 rm -f *.s *.ll *.riscv
+INCLUDES_NEWLIB="-I. -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/5.2.0/ -I $RISCV/riscv64-unknown-elf/include/c++/5.2.0/riscv64-unknown-elf/"
+INCLUDES_GLIBC="-I $RISCV/sysroot/usr/include/ -I $RISCV/riscv64-unknown-linux-gnu/include/c++/5.2.0/ -I $RISCV/riscv64-unknown-linux-gnu/include/c++/5.2.0/riscv64-unknown-linux-gnu"
 for x in Test SubclassTest; do
 	echo Building ${x}.cc with clang
 	clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S ${x}.cc -emit-llvm -o ${x}.ll -fno-exceptions -DCHECK_TAGS || exit 2
@@ -9,7 +11,7 @@ for x in Test SubclassTest; do
 	llc -use-init-array -filetype=asm -march=riscv -mcpu=LowRISC ${x}.opt.bc -o ${x}.opt.s || exit 4
 done
 for main in main-*.cc; do
-	for build in gcc clang gcc-linux
+	for build in gcc clang gcc-linux clang-linux
 	do
 		rm -f main*.s
 		case "$build" in
@@ -17,9 +19,14 @@ for main in main-*.cc; do
 			echo Building $main with gcc
 			riscv64-unknown-elf-gcc -fpermissive -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main -o ${main}.s || exit 1
 			;;
-		"clang")
+		"clang"|"clang-linux")
 			echo Building $main with clang
-			if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S $main -emit-llvm -o ${main}.ll -fno-exceptions -DCHECK_TAGS; then echo Failed to build $main with $build; break; fi
+			if test "$build" = "clang"; then
+				INCLUDES="$INCLUDES_NEWLIB"
+			else
+				INCLUDES="$INCLUDES_GLIBC"
+			fi
+			if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC $INCLUDES -S $main -emit-llvm -o ${main}.ll -fno-exceptions -DCHECK_TAGS; then echo Failed to build $main with $build; break; fi
 			if ! opt -load ../../../build/Debug+Asserts/lib/LLVMTagCodePointers.so -tag-code-pointers < ${main}.ll > ${main}.opt.bc ; then echo Failed to optimise $main; break; fi
 		        llvm-dis ${main}.opt.bc > ${main}.opt.ll
 			if ! llc -use-init-array -filetype=asm -march=riscv -mcpu=LowRISC ${main}.opt.bc -o ${main}.opt.s; then echo Failed to convert optimised $main to assembler; break; fi
@@ -29,9 +36,13 @@ for main in main-*.cc; do
 		"gcc-linux")
 			riscv64-unknown-linux-gnu-g++ -fno-stack-protector -O0 -static -o test-${main}.${build}.riscv $main Test.cc SubclassTest.cc || exit 4
 			;;
+		"clang-linux")
+			riscv64-unknown-linux-gnu-g++ -fno-stack-protector -O0 -static -o test-${main}.${build}.riscv *.s || exit 5
+			;;
 		*)
 			echo Assembling and linking with gcc
 			riscv64-unknown-elf-g++ -o test-${main}.${build}.riscv *.s || exit 3
+			;;
 		esac
 		echo Successfully built test-${main}.${build}.riscv
 	done
