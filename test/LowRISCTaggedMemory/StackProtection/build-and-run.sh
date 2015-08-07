c6b9f41510f68338830e819d07f2d03ac6f84e56
Matthew Toseland <mjt92@cam.ac.uk>
2015-08-07 10:52:07 +0100

    Stack protection clang linux tests work

--- test/LowRISCTaggedMemory/StackProtection/build-and-run.sh
+++ test/LowRISCTaggedMemory/StackProtection/build-and-run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if ! ./build.sh; then exit 1; fi
-if ! spike pk test-main-return.c.clang.riscv | grep "Success" > /dev/null 2>&1; then echo "TEST FAILED: main-return.c (should succeed)"; exit 2; fi
-if spike pk test-main-stack-protection.c.clang.riscv | grep "Success" > /dev/null 2>&1; then echo "TEST FAILED: main-stack-protection.c succeeded (should fail)"; fi
+if ! spike pk test-main-return.c.clang.riscv | grep "Success"; then echo "TEST FAILED: main-return.c with clang (should succeed)"; exit 2; fi
+if ! spike pk test-main-return.c.gcc.riscv | grep "Success"; then echo "TEST FAILED: main-return.c with gcc (should succeed)"; exit 2; fi
+if ! spike pk test-main-stack-protection.c.gcc.riscv | grep "Should have terminated by now"; then echo "GCC test didn't crash, maybe stack protector is enabled?"; exit 3; fi
+echo Stack protection failed on GCC as expected
+if ! spike pk test-main-stack-protection.c.clang.riscv | grep "Illegal store"; then echo "TEST FAILED: main-stack-protection.c succeeded (should fail)"; fi
+echo Stack protection succeeded on Clang
 echo All tests succeeded
 
--- test/LowRISCTaggedMemory/StackProtection/build.sh
+++ test/LowRISCTaggedMemory/StackProtection/build.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 # FIXME Build it all with clang when it works! :(
 rm -f *.s *.ll *.riscv
+INCLUDES_NEWLIB="-I. -I $RISCV/riscv64-unknown-elf/include/"
+INCLUDES_GLIBC="-I $RISCV/sysroot/usr/include/"
 for main in main*.c; do
-	for build in gcc clang gcc-linux
+	for build in gcc clang gcc-linux clang-linux
 	do
-		if test "$build" = "gcc"; then
+	rm *.s
+	case "$build" in
+		gcc)
 			echo Building $main with gcc
 			riscv64-unknown-elf-gcc -fpermissive -O0 -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S -fno-stack-protector $main -o ${main}.s || exit 1
-		else
+			;;
+		"clang"|"clang-linux")
 			echo Building $main with clang
+			if test "$build" = "clang"; then
+				INCLUDES="$INCLUDES_NEWLIB"
+			else
+				INCLUDES="$INCLUDES_GLIBC"
+			fi
 			if ! clang -O0 -target riscv -mcpu=LowRISC -mriscv=LowRISC -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/ -I $RISCV/riscv64-unknown-elf/include/ -I $RISCV/riscv64-unknown-elf/include/c++/4.9.2/riscv64-unknown-elf/ -S -fno-stack-protector $main -o ${main}.s; then echo Failed to build $main with $build; break; fi
-		fi
-		if test "$build" == "gcc-linux"; then
-			riscv64-unknown-linux-gnu-gcc -fno-stack-protector -O0 -static -o test-${main}.${build}.riscv $main || exit 4
-		else
+			;;
+		gcc-linux)
+			riscv64-unknown-linux-gnu-gcc -fno-stack-protector -O0 -static -S -o main.s $main || exit 4
+			;;
+		*)
+			echo "./build.sh BUILDTYPE"
+			echo "Where BUILDTYPE is:"
+			echo "  clang: Test LLVM with tags"
+			echo "  gcc: Test GCC without tags"
+			echo "  gcc-linux: Build test for booted Linux using GCC"
+			echo "  clang-linux: Build test for booted Linux using Clang with tags"
+			exit 1
+			;;
+	esac
+	case "$build" in
+		"clang"|"gcc")
 			echo Assembling and linking with gcc
 			riscv64-unknown-elf-g++ -O0 -o test-${main}.${build}.riscv ${main}.s || exit 3
-		fi
-		echo Successfully built test-${main}.${build}.riscv
+			;;
+		"clang-linux"|"gcc-linux")
+			riscv64-unknown-linux-gnu-gcc -static -o test-${main}.${build}.riscv *.s || exit 4
+			;;
+	esac
+	echo Successfully built test-${main}.${build}.riscv
 	done
 done
